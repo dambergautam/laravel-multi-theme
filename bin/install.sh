0828056420e95549a8daf552d0c706da57b03e08
Damber Prasad Gautam <damber@apnic.net>
2020-12-24 17:51:41 +1000

    Added laravel dir to git and updated install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clean up existing docker data -For clean installation
-rm -rf docker/laravel
+# rm -rf docker/laravel
 rm -rf docker/db/dbdata
 
 docker-compose down 
@@ -9,9 +9,9 @@ docker-compose down
 # OPTION 1
 # -------------------- #
 # Download laravel LTS framework (https://github.com/laravel/laravel/releases)
-LARAVEL_VERSION_NO="6.0.0"
-curl -L https://github.com/laravel/laravel/archive/v${LARAVEL_VERSION_NO}.tar.gz | tar xz
-mv laravel-${LARAVEL_VERSION_NO} docker/laravel
+# LARAVEL_VERSION_NO="6.0.0"
+# curl -L https://github.com/laravel/laravel/archive/v${LARAVEL_VERSION_NO}.tar.gz | tar xz
+# mv laravel-${LARAVEL_VERSION_NO} docker/laravel
 cp docker/laravel/.env.example docker/laravel/.env
 # -------------------- #
 
@@ -35,7 +35,7 @@ APP_NAME="laravel_php_fpm"
 
 docker-compose exec $APP_NAME composer install
 
-docker-compose exec $APP_NAME php artisan key:generate
+# docker-compose exec $APP_NAME php artisan key:generate
 
 docker-compose exec $APP_NAME node -v
 
@@ -46,8 +46,8 @@ docker-compose exec $APP_NAME npm run dev
 # Ensure database is ready to handel php artisan command
 sleep 3
 
-docker-compose exec $APP_NAME php artisan optimize
+# docker-compose exec $APP_NAME php artisan optimize
 
-docker-compose exec $APP_NAME php artisan migrate
+# docker-compose exec $APP_NAME php artisan migrate
 
 docker-compose exec $APP_NAME vendor/bin/phpunit
